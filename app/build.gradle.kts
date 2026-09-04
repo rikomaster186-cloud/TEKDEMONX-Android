f9cc58aa68b351f81cad18b7e06051bf642f94f7
rikomaster186-cloud <rikomaster186@gmail.com>
2026-09-04 23:39:27 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,9 +15,19 @@ android {
         versionName = "1.0.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("../pubguc-release.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
